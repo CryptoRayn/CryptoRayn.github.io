91dc26853761a84dc00dc1c3da8b3e595651df0d
CryptoRayn <Mastersonnic@gmail.com>
2025-05-06 19:12:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.getElementById("claimButton").addEventListener("click", function () {
+  const messages = [
+    "¡Felicidades! Has recibido 10 satoshis 🎉",
+    "¡Buen intento! Inténtalo de nuevo en 5 minutos ⏳",
+    "¡Wow! Te ganaste 50 satoshis 🔥",
+    "Lo siento, sin recompensa esta vez 😢"
+  ];
+
+  const msg = messages[Math.floor(Math.random() * messages.length)];
+  document.getElementById("message").innerText = msg;
+});
 
